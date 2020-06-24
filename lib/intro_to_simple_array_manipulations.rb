58e90b13e089c931dde68ee3644e2899b74a570d
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-06-24 21:05:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,1 @@
-def using_concat(array)
-  count = 0
-  new_array = []
-  while count < array.length do
-    new_array.push(array[count].concat)
-    count += 1
-  end
-  new_array
-end
+using_concat.concat
